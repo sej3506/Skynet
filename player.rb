d98e35cdcd6a20b9e9e7841c7a27e075baf9cc01
Sara Jackson <sara@testdouble.com>
2020-04-07 14:54:04 -0400

    Player now moves towards closest resource.

--- player.rb
+++ player.rb
@@ -1,8 +1,8 @@
 require 'rest-client'
 require 'json'
 
-$team = '39'
-$host = 'http://retreat-game.herokuapp.com'
+$team = '16'
+$host = 'https://6f3726fb.ngrok.io'
 $directions = ['NORTH', 'SOUTH', 'WEST', 'EAST']
 
 def move direction
@@ -21,7 +21,7 @@ def create name, water, food, stamina, strength
 end
 
 def calculate_resource_score board
-  board.reduce { |scores, coord|
+  board.reduce({}) { |scores, coord|
     key = "#{coord['x']},#{coord['y']}"
     score = (scores[key] || 0)
     if coord['is_food']
@@ -29,6 +29,39 @@ def calculate_resource_score board
     end
     scores.update({key => score})
   }
+end
+
+def calculate_closest_resource player_board
+  # TODO Need to calculate which resource to go to based on need of player.
+  player_location = { 'x' => player_board['player']['x'], 'y' => player_board['player']['y'] }
+ 
+  resource_distances = player_board['board'].map { |location| 
+    {
+      'x' => location['x'],
+      'y' => location['y'], 
+      'resource' => location['is_food'] ? "🌴" : "💧",
+      'distance' => (player_location['x'] + player_location['y'] - (location['x'] + location['y'])).abs
+    } unless location['is_player']
+  }
+  resource_distances.min_by { |entry| entry['distance'] }
+end
+
+def calculate_direction_to_move player_location, other_location
+  if player_location['x'] > other_location['x']
+    'WEST'
+  elsif player_location['x'] < other_location['x']
+    'EAST'
+  elsif player_location['y'] > other_location['y']
+    'NORTH'
+  elsif player_location['y'] < other_location['y']
+    'SOUTH'
+  else
+    $directions.sample
+  end
+end
+
+def get_location thing
+  { 'x' => thing['x'], 'y' => thing['y'] }
 end
 
 name = "Bob"
@@ -45,11 +78,23 @@ else
   exit
 end
 
+puts calculate_closest_resource player_board
+
 while player_board['player']['active'] do
-  player_board.update(move $directions.sample)
+  location_to_move_toward = calculate_closest_resource player_board
+
+  if location_to_move_toward 
+    puts "Moving to #{location_to_move_toward}"
+    direction = calculate_direction_to_move (get_location player_board['player']), location_to_move_toward
+  else 
+    direction = $directions.sample
+    puts "Moving randomly"
+  end
+  puts "\t-> #{direction}"
+  player_board.update move direction
 
   puts "#{name} is now at (#{player_board['player']['x']}, #{player_board['player']['y']})"
-  puts calculate_resource_score player_board['board']
+  puts "\n\n#{name}'s 🌡: #{player_board['player']}"
   sleep 2
 end
 
